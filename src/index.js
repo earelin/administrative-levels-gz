131f81a0c6112c5fd058a415fddc0a3f78bf7baf
earelin <xavier.carriba@gmail.com>
2022-08-08 23:04:40 +0100

    Integrating divisions with the CLI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+const nomenclatorParser = require('./nomenclator-parser');
 const fileName = process.argv[2];
 
 if (!fileName) {
   console.log('Please pass the nomenclator.csv file as first argument');
-  process.exitCode = 9;
-} else {
-  console.log('file: ', process.argv[2]);
+  process.exit(9);
 }
+
+nomenclatorParser(process.argv[2])
+  .then(divisions => console.log(JSON.stringify(divisions.toArray())));
