7a7f7fb5c55f0fea5df38854cff39214fc80b667
earelin <xavier.carriba@gmail.com>
2022-08-10 22:28:53 +0100

    Adding infrastructure as code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,17 @@
-provider "google" {
-  project = "earelin"
-  region = "europe-west2"
-}
-
 terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+
   backend "gcs" {
     bucket = "earelin-terraform-status"
     prefix = "administrative-levels-gz/prod"
   }
+}
+
+provider "google" {
+  project = "earelin"
+  region = "europe-west2"
 }
