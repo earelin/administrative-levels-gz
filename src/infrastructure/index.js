7ca7d71c79d73246fed9f53fcd3e1d184af9058c
earelin <xavier.carriba@gmail.com>
2022-08-11 03:55:37 +0100

    Refactoring exports

--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -1,6 +1,8 @@
 const LevelsRepository = require('./levels-repository');
 const divisions = require(process.env.DATA_LEVELS_PATH);
 
+const levelsRepository = new LevelsRepository(divisions);
+
 module.exports = {
-  levelsRepository: new LevelsRepository(divisions)
+  levelsRepository
 };
